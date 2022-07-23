4ac84c5290dc67b42f95e55d89948a6fbd5b9862
Fabricio <flaureano18@gmail.com>
2022-07-22 19:54:42 -0600

    Se agrego un Hola

--- src/operadoresaritmeticos/OperadoresAritmeticos.java
+++ src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -16,6 +16,8 @@ public class OperadoresAritmeticos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
+        System.out.println("HOlaaaaa");
+        System.out.println("holaa");
     }
     
 }
